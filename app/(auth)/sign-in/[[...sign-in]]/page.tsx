4bfee7e68a6e948f46712c0863670029a243d8c5
Lokesh Raj <lokeshraj851134@gmail.com>
2025-02-18 16:41:52 +0530

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -8,6 +8,8 @@ const SignInPage = () => {
       <div className="h-full flex-col items-center justify-center px-4 lg:flex">
         <div className="space-y-4 pt-16 text-center">
           <h1 className="text-3xl font-bold text-[#2E2A47]">Welcome back!</h1>
+          <h1 className="text-3xl font-bold text-[#2E2A47]">For login user id:lokesh , password:1234</h1>
+
           <p className="text-base text-[#7E8CA0]">
             Log in or create account to get back to your dashboard.
           </p>
